eded36e1d77b7782cc3aa99f63b2d9c6e0e9ed12
Donna <dlambertpdx@gmail.com>
2017-08-28 13:58:30 -0700

    refactor resetFields jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,14 @@ Address.prototype.fullAddress = function() {
   return this.street + ", " + this.city + ", " + this.state;
 }
 
+function resetFields() {
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
+}
+
 // user interface logic
 $(document).ready(function() {
 
@@ -64,11 +72,6 @@ $(document).ready(function() {
       $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
     });
   });
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
+    resetFields();
   });
 });
